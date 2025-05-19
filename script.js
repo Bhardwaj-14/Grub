20c876d08025f46b1849b2039f250917af776e3b
Bhardwaj-14 <bps.mbp2012@gmail.com>
2025-05-19 19:02:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-    const toggleButton = document.querySelector('nav button');
-    const navLinks = document.createElement('div');
+const toggleButton = document.querySelector('nav button');
+const navLinks = document.createElement('div');
 
-    navLinks.classList.add('mobile-menu', 'flex', 'flex-col', 'space-y-4', 'mt-4', 'md:hidden');
-    navLinks.innerHTML = `
-        <a href="#" class="hover:text-green-200">Home</a>
-        <a href="#" class="hover:text-green-200">About</a>
-        <a href="#" class="hover:text-green-200">Blog</a>
-        <a href="#" class="hover:text-green-200">Tips</a>
-        <a href="#" class="hover:text-green-200">Contact</a>
-    `;
+navLinks.classList.add('mobile-menu', 'flex', 'flex-col', 'space-y-4', 'mt-4', 'md:hidden');
+navLinks.innerHTML = `
+    <a href="#" class="hover:text-green-200">Home</a>
+    <a href="#" class="hover:text-green-200">About</a>
+    <a href="#" class="hover:text-green-200">Blog</a>
+    <a href="#" class="hover:text-green-200">Tips</a>
+    <a href="#" class="hover:text-green-200">Contact</a>
+`;
 
-    let isOpen = false;
-    toggleButton.addEventListener('click', () => {
-        if (!isOpen) {
-            toggleButton.parentNode.appendChild(navLinks);
-        } else {
-            navLinks.remove();
-        }
-        isOpen = !isOpen;
-    });
+let isOpen = false;
+toggleButton.addEventListener('click', () => {
+    if (!isOpen) {
+        toggleButton.parentNode.appendChild(navLinks);
+    } else {
+        navLinks.remove();
+    }
+    isOpen = !isOpen;
+});
