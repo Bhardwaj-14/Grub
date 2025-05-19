e013fc6d6764301fd3031bc7e3004590d8fdbd3f
Bhardwaj-14 <bps.mbp2012@gmail.com>
2025-05-19 19:02:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const navLinks = document.createElement('div');
 
 navLinks.classList.add('mobile-menu', 'flex', 'flex-col', 'space-y-4', 'mt-4', 'md:hidden');
 navLinks.innerHTML = `
-    <a href="#" class="hover:text-green-200">Home</a>
-    <a href="#" class="hover:text-green-200">About</a>
-    <a href="#" class="hover:text-green-200">Blog</a>
-    <a href="#" class="hover:text-green-200">Tips</a>
-    <a href="#" class="hover:text-green-200">Contact</a>
+    <a href="#home" class="hover:text-green-200">Home</a>
+    <a href="#about" class="hover:text-green-200">About</a>
+    <a href="#blog" class="hover:text-green-200">Blog</a>
+    <a href="#tips" class="hover:text-green-200">Tips</a>
+    <a href="#contact" class="hover:text-green-200">Contact</a>
 `;
 
 let isOpen = false;
